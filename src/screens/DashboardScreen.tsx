911080b02e7d17fab742fb399b348c62b8c75045
Dhairya Sehgal <dhairyasehgal@Dhairyas-MacBook-Air.local>
2025-03-15 18:19:49 +0530

    feat(theme): set up shadcn/ui and applied green theme
    - Installed and configured shadcn/ui
    - Customized the default theme to use a green color palette
    - Updated global styles in
    - Ensured proper theming for light and dark modes

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 const DashboardScreen = () => {
-  return <div>DashboardScreen</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
 };
 
 export default DashboardScreen;
