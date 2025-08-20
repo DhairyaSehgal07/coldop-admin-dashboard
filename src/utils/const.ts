3e1c914c466a50e0d52719aba41aaa8568168e68
Dhairya Sehgal <dhairyasehgal2307@gmail.com>
2025-08-20 11:44:40 +0530

    chore: update base url

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,4 @@
 const BASE_URL =
-  "https://coldop-backend-cdeteah5aherhubj.centralindia-01.azurewebsites.net/";
+  "https://coldop-backend-cdeteah5aherhubj.centralindia-01.azurewebsites.net/api/super-admin";
 
 export { BASE_URL };
